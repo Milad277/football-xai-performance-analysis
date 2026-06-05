6111f7f14e83cf4335db3d13b5868be5eb3f1923
Milad277 <125996024+Milad277@users.noreply.github.com>
2026-06-05 14:18:05 +0200

    Document SQL schema for football event data
    Added comments to describe the SQL schema and main tables.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,12 @@
+-- SQL schema for football event data
 
+-- This file will contain the database schema used to create
+-- the relational SQLite database for the football analytics pipeline.
+
+-- Main expected tables:
+-- matches
+-- teams
+-- players
+-- events
+-- event_positions
+-- lineups
